8c9a127622e6910cf057a18d3a5cbcfa760cced9
whojes <whojes@gmail.com>
2018-01-22 01:41:41 +0900

    [main]

--- src/main.go
+++ src/main.go
@@ -1,45 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"encoding/json"
-	"net/http"
-	"time"
-	"dbfunc"
 	"database/sql"
+	"dbfunc"
+	"encoding/json"
+	"fmt"
+	"net/http"
 	"strconv"
+	"time"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type CONSTANTS struct {
 	ACCESS_TOKEN string
-	SECRET_KEY string
-	BASE_URL string
+	SECRET_KEY   string
+	BASE_URL     string
 }
 
 type PAYLOAD struct {
 	Currency string
-	Period string
+	Period   string
 }
 
 type RESBODY struct {
-	ErrorCode string
-	Timestamp string
-	CompleteOrders[] struct {
+	ErrorCode      string
+	Timestamp      string
+	CompleteOrders []struct {
 		Timestamp string
-		Price string
-		Qty string
+		Price     string
+		Qty       string
 	}
 }
 
 func main() {
 	constants := CONSTANTS{
-		ACCESS_TOKEN : "6ef090a1-ad27-4123-afe7-7743e84c2231",
-		SECRET_KEY : "a21cf668-cd9f-4be3-abc8-c66580ceb813",
-		BASE_URL : "https://api.coinone.co.kr",
+		ACCESS_TOKEN: "6ef090a1-ad27-4123-afe7-7743e84c2231",
+		SECRET_KEY:   "a21cf668-cd9f-4be3-abc8-c66580ceb813",
+		BASE_URL:     "https://api.coinone.co.kr",
 	}
 	db, err := sql.Open("mysql", dbfunc.DBUSER+":"+dbfunc.DBAUTH+"@tcp("+dbfunc.DBIPADDR+":"+dbfunc.DBPORT+")/btc")
-	err2 := db.Ping()	
+	err2 := db.Ping()
 	if err != nil {
 		panic(err.Error)
 	} else if err2 != nil {
@@ -49,13 +50,12 @@ func main() {
 		fmt.Print("DB Connected.")
 	}
 
-	go constants.getCoinData("BTC", 10, db)
-	fmt.Scanln()
+	constants.getCoinData("BTC", 10, db)
 }
 
-func (c *CONSTANTS) getCoinData(s string, duration int, db *sql.DB){
+func (c *CONSTANTS) getCoinData(s string, duration int, db *sql.DB) {
 	url := c.BASE_URL + "/trades"
-	for{
+	for {
 		res, err := http.Get(url)
 		if err != nil {
 			fmt.Print(err)
@@ -64,12 +64,12 @@ func (c *CONSTANTS) getCoinData(s string, duration int, db *sql.DB){
 			err2 := json.NewDecoder(res.Body).Decode(&resbody)
 			if err2 == nil {
 				price := resbody.refine()
-				price.Insert(db);
+				price.Insert(db)
 			} else {
 				fmt.Print(err2)
 			}
 		}
-		time.Sleep(time.Duration(duration * 60) * time.Second)
+		time.Sleep(time.Duration(duration*60) * time.Second)
 	}
 }
 
@@ -77,13 +77,13 @@ func (r *RESBODY) refine() *dbfunc.PRICE {
 	total := 0.0
 	price := new(dbfunc.PRICE)
 	lastOrder := len(r.CompleteOrders)
-	for i := lastOrder-1 ; ; i-- {
+	for i := lastOrder - 1; ; i-- {
 		co := r.CompleteOrders[i]
 		qty, _ := strconv.ParseFloat(co.Qty, 64)
 		ts, _ := strconv.ParseUint(co.Timestamp, 10, 64)
 		total = total + qty
-		if i == lastOrder -1  {
-			price.Timestamp, _= strconv.ParseUint(co.Timestamp, 10, 64)
+		if i == lastOrder-1 {
+			price.Timestamp, _ = strconv.ParseUint(co.Timestamp, 10, 64)
 			price.LastPrice, _ = strconv.ParseUint(co.Price, 10, 64)
 			price.MaxPrice = price.LastPrice
 			price.MinPrice = price.LastPrice
@@ -96,7 +96,7 @@ func (r *RESBODY) refine() *dbfunc.PRICE {
 			curPrice, _ := strconv.ParseUint(co.Price, 10, 64)
 			if curPrice > price.MaxPrice {
 				price.MaxPrice = curPrice
-			} 
+			}
 			if curPrice < price.MinPrice {
 				price.MinPrice = curPrice
 			}
