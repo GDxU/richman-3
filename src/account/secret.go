18420d90757c01d627388291180b568f6f18aafe
whojes <whojes@gmail.com>
2018-02-19 18:35:42 +0900

    testwq q

--- src/account/secret.go
+++ src/account/secret.go
@@ -1,6 +1,8 @@
 package account
 
 // Secret values of accounts
+// after change the values,
+// git update-index --assume-unchanged src/dbfunc/secret.go
 const (
 	AccessToken string = "your_accessToken"
 	SecretKey   string = "your_secretKey"
